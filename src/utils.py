0f371573390b546a8c4a67688da973557f65a336
GR20204 <newaccforgre123@gmail.com>
2026-01-18 22:40:14 +0530

    commit 5

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import os
 import sys
-import numpy as np
-import pandas as pd
+from dataclasses import dataclass
+
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 import dill as dl
+from sklearn.model_selection import train_test_split
 
 def save_object(file_path,obj):
     try:
@@ -15,3 +17,17 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
+def evaluate_models(xtrain,ytrain,xtest,ytest,models):
+    try: 
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(xtrain,ytrain)
+            ytrainpred = model.predict(xtrain)
+            ytestpred = model.predict(xtest)
+            train_model_score = r2_score(ytrain,ytrainpred)
+            test_model_score = r2_score(ytest,ytestpred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
